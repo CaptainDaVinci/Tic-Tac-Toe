44ece15c325725e7eebece7a0c9296d844f64c33
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-07 22:32:57 +0530

    Improved code after code review from http://codereview.stackexchange.com/questions/154568/single-player-tic-tac-toe-game/154604#154604

--- src/Multi-Player.c
+++ src/Multi-Player.c
@@ -6,71 +6,313 @@
 #define GAMES 3
 #define WIN 2
 #define LOSS -1
-
+#define DRAW 0
 // Adds colour to the output.
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-bool gameStatus(char [][3]);
-void display(char [][3]);
-void startGame(char [][3]);
-void gameReset(char [][3]);
-void scoreBoard(void);
 
-struct Player
+typedef struct
 {
     int score[GAMES];
     char choice;
     char name[10];
     bool turn;
 
-}player1 = {{0}, 'O', "xyz", false}, player2 = {{0}, 'X', "abc", false};
+}Player;
 
-// keeps track of all the valid positions left in the game.
-int games = 0;
+typedef struct
+{
+    char board[3][3];
+    bool validPos[10];
+    int games;
 
-bool validPos[10] = {false, true, true, true, true, true, true, true, true, true};
+}Game;
+
+void assignXO(Player *, Player *);
+void startGame(Game *, Player *, Player *);
+void display(Game *, Player *, Player *);
+bool victoryCheck(Game *);
+bool isComplete(Game *);
+void playerMove(Game *, Player *);
+void declareWinner(Game *, Player *, Player *);
+void gameReset(Game *);
+void scoreBoard(Player *, Player *, int);
 
 int main(void)
 {
-    // A two-dimensional array depicting the game.
-    char game[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     char choice;
+    Game *game = malloc(sizeof(Game));
+    Player *player_1 = malloc(sizeof(Player));
+    Player *player_2 = malloc(sizeof(Player));
 
-    printf("Enter Player 1's name : ");
-    scanf("%s", player1.name);
-    printf("Enter Player 2's name : ");
-    scanf("%s", player2.name);
+    gameReset(game);
+    game->games = 0;
 
-    printf("\n%s, ", player1.name);
+    assignXO(player_1, player_2);
+    startGame(game, player_1, player_2);
+    scoreBoard(player_1, player_2, game->games);
+
+    while(game->games < GAMES)
+    {
+        printf("Play again ? (Y/N)\n");
+        if(scanf(" %c", &choice) == 1);
+
+        if(choice == 'y' || choice == 'Y')
+        {
+            gameReset(game);
+            startGame(game, player_1, player_2);
+            scoreBoard(player_1, player_2, game->games);
+        }
+
+        else
+            break;
+    }
+
+    free(game);
+    free(player_1);
+    free(player_2);
+}
+
+void assignXO(Player *player_1, Player *player_2)
+{
+    char choice;
+    int errorCheck;
+
+    do
+    {
+        printf("Enter Player 1's name : ");
+        errorCheck = scanf("%s", player_1->name);
+
+    } while(errorCheck != 1);
+
+    do
+    {
+        printf("Enter Player 1's name : ");
+        errorCheck = scanf("%s", player_2->name);
+
+    } while(errorCheck != 1);
+
+    printf("\n%s, ", player_1->name);
+
     do
     {
         printf("X or O ?\n");
-        scanf(" %c", &player1.choice);
 
-    }while(player1.choice != 'X' && player1.choice != 'O' && player1.choice != 'x' && player1.choice != 'o');
+        errorCheck = scanf(" %c", &choice);
+
+    }while((choice != 'X' && choice != 'O' && choice != 'x' && choice != 'o') || errorCheck != 1);
 
     // assigns X or O to the two players.
-    player1.choice = toupper(player1.choice);
+    player_1->choice = toupper(choice);
 
-    if(player1.choice == 'x' || player1.choice == 'X')
+    if(player_1->choice == 'X')
     {
-        player1.choice = 'X';
-        player2.choice = 'O';
+        player_1->choice = 'X';
+        player_2->choice = 'O';
+    }
+    
+    else
+    {
+        player_2->choice = 'X';
+        player_1->choice = 'O';
     }
 
-    display(game);
+    player_1->turn = true;
+    player_2->turn = false;
+}
 
-    player1.turn = true;
-    player2.turn = false;
-    startGame(game);
+void startGame(Game *game, Player *player_1, Player *player_2)
+{
+    display(game, player_1, player_2);
+
+    while(!victoryCheck(game) && !isComplete(game))
+    {
+        if(player_1->turn)
+            playerMove(game, player_1);
+
+        else
+            playerMove(game, player_2);
+
+        player_1->turn = !player_1->turn;
+        player_2->turn = !player_2->turn;
+
+        display(game, player_1, player_2);
+    }
+
+    declareWinner(game, player_1, player_2);
+}
+
+void playerMove(Game *game, Player *player)
+{
+    int i, j;
+    int errorCheck;
+    char pos;
+
+    do
+    {
+        printf("%s's turn : ", player->name);
+        errorCheck = scanf(" %c", &pos);
+
+    }while(!isdigit(pos) || !game->validPos[pos - '0'] || errorCheck != 1);
+
+    game->validPos[pos - '0'] = false;
+
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            if(game->board[i][j] == pos)
+                    game->board[i][j] = player->choice;
+}
+
+// checks if the game is completed when there are 3 consecutive X or O
+// along a row, coloumn or either of the diagonals.
+bool victoryCheck(Game *game)
+{
+    int i, j;
+
+    // checks along a row.
+    for(i = 0, j = 0; i < 3; i++)
+        if(game->board[i][j] == game->board[i][j + 1] && game->board[i][j] == game->board[i][j + 2])
+            return true;
+
+    // checks along a coloumn.
+    for(j = 0, i = 0; j < 3; j++)
+        if(game->board[i][j] == game->board[i + 1][j] && game->board[i][j] == game->board[i + 2][j])
+            return true;
+
+    // checks along right diagonal.
+    if(game->board[0][0] == game->board[1][1] && game->board[0][0] == game->board[2][2])
+        return true;
+
+    // checks along left diagonal.
+    if(game->board[0][2] == game->board[1][1] && game->board[0][2] == game->board[2][0])
+        return true;
+
+    return false;
+}
+
+bool isComplete(Game *game)
+{
+    int i, j;
+
+    for(i = 1; i < 10; i++)
+    {
+        if(game->validPos[i])
+            return false;
+    }
+
+    return true;
+}
+
+void declareWinner(Game *game, Player *player_1, Player *player_2)
+{
+    int i, j;
+
+    // checks along a row.
+    for(i = 0, j = 0; i < 3; i++)
+    {
+        if(game->board[i][0] == game->board[i][1] && game->board[i][0] == game->board[i][2])
+        {
+            if(game->board[i][j] == player_1->choice)
+            {
+                printf("%s Wins!\n", player_1->name);
+                player_1->score[game->games] = WIN;
+                player_2->score[game->games] = LOSS;
+            }
+
+            else
+            {
+                printf("%s Wins!\n", player_2->name);
+                player_2->score[game->games] = WIN;
+                player_1->score[game->games] = LOSS;
+            }
+
+            game->games++;
+            return;
+        }
+    }
+
+    // checks along a coloumn.
+    for(j = 0, i = 0; j < 3; j++)
+    {
+        if(game->board[0][j] == game->board[1][j] && game->board[0][j] == game->board[2][j])
+        {
+            if(game->board[0][j] == player_1->choice)
+            {
+                printf("%s Wins!\n", player_1->name);
+                player_1->score[game->games] = WIN;
+                player_2->score[game->games] = LOSS;
+            }
+
+            else
+            {
+                printf("%s Wins!\n", player_2->name);
+                player_2->score[game->games] = WIN;
+                player_1->score[game->games] = LOSS;
+            }
+
+            game->games++;
+            return;
+        }
+    }
+
+    // checks along right diagonal.
+    if(game->board[0][0] == game->board[1][1] && game->board[0][0] == game->board[2][2])
+    {
+        if(game->board[0][0] == player_1->choice)
+        {
+            printf("%s Wins!\n", player_1->name);
+            player_1->score[game->games] = WIN;
+            player_2->score[game->games] = LOSS;
+        }
+
+        else
+        {
+            printf("%s Wins!\n", player_2->name);
+            player_2->score[game->games] = WIN;
+            player_1->score[game->games] = LOSS;
+        }
+        game->games++;
+        return;
+    }
+
+    // checks along left diagonal.
+    if(game->board[0][2] == game->board[1][1] && game->board[0][2] == game->board[2][0])
+    {
+        if(game->board[0][2] == player_1->choice)
+        {
+            printf("%s Wins!\n", player_1->name);
+            player_1->score[game->games] = WIN;
+            player_2->score[game->games] = LOSS;
+        }
+
+        else
+        {
+            printf("%s Wins!\n", player_2->name);
+            player_2->score[game->games] = WIN;
+            player_1->score[game->games] = LOSS;
+        }
+        game->games++;
+        return;
+    }
+
+    if(isComplete(game))
+    {
+        printf("Game Over !\n");
+        printf("No more possible moves\n");
+
+        player_1->score[game->games] = DRAW;
+        player_2->score[game->games] = DRAW;
+
+        game->games++;
+        return ;
+    }
 }
 
 // displays the game's current status.
-void display(char game[][3])
+void display(Game *game, Player *player_1, Player * player_2)
 {
-    char choice;
     int i, j;
 
     // use system("cls"); if using windows.
@@ -81,240 +323,91 @@ void display(char game[][3])
     {
         for(j = 0; j < 3; j++)
         {
-            if(game[i][j] == 'X')
+            if(game->board[i][j] == 'X')
                 printf(ANSI_COLOR_RED "\tX" ANSI_COLOR_RESET);
 
-            else if(game[i][j] == 'O')
+            else if(game->board[i][j] == 'O')
                 printf(ANSI_COLOR_GREEN "\tO" ANSI_COLOR_RESET);
 
             else
-                printf("\t%c", game[i][j]);
+                printf("\t%c", game->board[i][j]);
         }
         printf("\n\t------------------\n");
     }
+    printf("\n%s - %c\t", player_1->name, player_1->choice);
+    printf("%s - %c\n", player_2->name, player_2->choice);
 }
 
-void startGame(char game[3][3])
-{
-    int i, j;
-    char pos;
-    char choice;
-    printf("\n%s - %c\t", player1.name, player1.choice);
-    printf("%s - %c\n", player2.name, player2.choice);
-
-    // if it is player 1's turn, check for valid input.
-    if(player1.turn)
-    {
-        do
-        {
-            printf("%s's turn : ", player1.name);
-            scanf(" %c", &pos);
-
-        }while(!isdigit(pos) || !validPos[pos - '0']);
-    }
-
-    // if it is player 2's turn, check for valid input.
-    if(player2.turn)
-    {
-        do
-        {
-            printf("%s's turn : ", player2.name);
-            scanf(" %c", &pos);
-
-        }while(!isdigit(pos) || !validPos[pos - '0']);
-
-    }
-    // update the valid positions availabe.
-    validPos[pos - '0'] = false;
-
-    // Assigns either X or O to the spot chosen.
-    for(i = 0; i < 3; i++)
-    {
-        for(j = 0; j < 3; j++)
-        {
-            if(game[i][j] == pos)
-            {
-                if(player1.turn)
-                    game[i][j] = player1.choice;
-
-                else
-                    game[i][j] = player2.choice;
-            }
-        }
-    }
-
-    display(game);
-
-    // checks if the any has won the game or not also checks if there are any
-    // further possible moves.
-    // also acts as the base case for the game to end.
-    if(gameStatus(game))
-    {
-        if(player1.turn)
-        {
-            printf("\n%s WINS !\n", player1.name);
-            player1.score[games] = WIN;
-            player2.score[games] = LOSS;
-        }
-
-        else
-        {
-            printf("\n%s WINS !\n", player2.name);
-            player2.score[games] = WIN;
-            player1.score[games] = LOSS;
-        }
-
-        scoreBoard();
-
-        printf("Play again ? (Y/N)\n");
-        scanf(" %c", &choice);
-
-        if(choice == 'y' || choice == 'Y')
-            gameReset(game);
-
-        return;
-    }
-
-    // swap the turns
-    player1.turn = !player1.turn;
-    player2.turn = !player2.turn;
-
-    // recursive call to continue the game.
-    startGame(game);
-}
-
-// checks if the game is completed when there are 3 consecutive X or O
-// along a row, coloumn or either of the diagonals.
-bool gameStatus(char game[][3])
-{
-    char choice;
-    int i, j;
-    int flag = 0;
-
-    // checks if there are any further possible moves.
-    for(i = 1; i < 10; i++)
-    {
-        if(validPos[i])
-        {
-            flag = 1;
-            break;
-        }
-    }
-
-    // if there are no more possible moves then the game is over.
-    if(flag == 0)
-    {
-        scoreBoard();
-
-        printf("Game Over!\nNo more possible moves\n");
-        printf("Play again ? (Y/N)\n");
-        scanf(" %c", &choice);
-
-        if(choice == 'y' || choice == 'Y')
-            gameReset(game);
-
-        exit(0);
-    }
-
-    // checks along a row.
-    for(i = 0, j = 0; i < 3; i++)
-        if(game[i][j] == game[i][j + 1] && game[i][j] == game[i][j + 2])
-            return true;
-
-    // checks along a coloumn.
-    for(j = 0, i = 0; j < 3; j++)
-        if(game[i][j] == game[i + 1][j] && game[i][j] == game[i + 2][j])
-            return true;
-
-    // checks along right diagonal.
-    if(game[0][0] == game[1][1] && game[0][0] == game[2][2])
-        return true;
-
-    // checks along left diagonal.
-    if(game[0][2] == game[1][1] && game[0][2] == game[2][0])
-        return true;
-
-    return false;
-}
-
-void gameReset(char game[][3])
+void gameReset(Game *game)
 {
     int i, j;
     char k = '1';
 
     for(i = 1; i < 10; i++)
-        validPos[i] = true;
+        game->validPos[i] = true;
 
     for(i = 0; i < 3; i++)
     {
         for(j = 0; j < 3; j++)
         {
-            game[i][j] = k;
+            game->board[i][j] = k;
             k++;
         }
     }
-
-    display(game);
-
-    player1.turn = !player1.turn;
-    player2.turn = !player2.turn;
-
-    startGame(game);
 }
 
-void scoreBoard(void)
+void scoreBoard(Player *player_1, Player *player_2, int games)
 {
-    games++;
-
     int i;
     int totalScore1 = 0;
     int totalScore2 = 0;
 
-    printf("\n================\n");
-    printf("  LEADERBOARD\n");
-    printf("================\n");
-
-    for(i = 0; i < GAMES; i++)
-    {
-        totalScore1 += player1.score[i];
-        totalScore2 += player2.score[i];
-
-    }
-
-    printf("%s[%d]  %s[%d]\n\n", player1.name, totalScore1, player2.name, totalScore2);
+    printf("\n==================\n");
+    printf("   LEADERBOARD\n\n");
+    // printf("==================\n");
+    printf("   Game (%d / %d)\n", games, GAMES);
+    printf("==================\n");
 
     for(i = 0; i < games; i++)
     {
-        if(player1.score[i] == WIN)
-            printf(ANSI_COLOR_GREEN "  %d", WIN);
+        totalScore1 += player_1->score[i];
+        totalScore2 += player_2->score[i];
+
+    }
+
+    printf("%s[%d]  %s[%d]\n\n", player_1->name, totalScore1, player_2->name, totalScore2);
+
+    for(i = 0; i < games; i++)
+    {
+        if(player_1->score[i] == WIN)
+            printf(ANSI_COLOR_GREEN "  %2d", WIN);
 
         else
-            printf(ANSI_COLOR_RED "  %d", LOSS);
+            printf(ANSI_COLOR_RED "  %2d", LOSS);
 
         printf(ANSI_COLOR_RESET "\t");
 
-        if(player2.score[i] == WIN)
-            printf(ANSI_COLOR_GREEN "   %d", WIN);
+        if(player_2->score[i] == WIN)
+            printf(ANSI_COLOR_GREEN "   %2d", WIN);
 
 
         else
-            printf(ANSI_COLOR_RED "   %d", LOSS);
+            printf(ANSI_COLOR_RED "   %2d", LOSS);
 
         printf(ANSI_COLOR_RESET "\n");
     }
 
+    printf("==================\n");
     printf("\n");
     if(games + 1 > GAMES)
     {
         if(totalScore1 > totalScore2)
-            printf("%s WINS THE SERIES !\n", player1.name);
+            printf("%s WINS THE SERIES !\n", player_1->name);
 
         else if(totalScore2 > totalScore1)
-            printf("%s WINS THE SERIES !\n", player2.name);
+            printf("%s WINS THE SERIES !\n", player_2->name);
 
         else
             printf("NO RESULT!\n");
-
-        exit(0);
     }
 }
