78f27a8691c7e0cf3ce16ed3005fbc5f78c004ac
CaptainDaVinci <yashkothari199767@gmail.com>
2017-02-07 23:10:11 +0530

    A number of changes to improve the code structure after code review from http://codereview.stackexchange.com/questions/154568/single-player-tic-tac-toe-game/154604#154604

--- src/Multi-Player.c
+++ src/Multi-Player.c
@@ -3,15 +3,16 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-#define GAMES 3
-#define WIN 2
-#define LOSS -1
-#define DRAW 0
+// points for each result.
+#define WIN     2
+#define LOSS   -1
+#define DRAW    0
+#define GAMES   3
+
 // Adds colour to the output.
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
 
 typedef struct
 {
@@ -47,23 +48,25 @@ int main(void)
     Player *player_1 = malloc(sizeof(Player));
     Player *player_2 = malloc(sizeof(Player));
 
-    gameReset(game);
+    // sets the number of games played to zero.
     game->games = 0;
 
     assignXO(player_1, player_2);
     startGame(game, player_1, player_2);
-    scoreBoard(player_1, player_2, game->games);
 
+    // continues to ask user if he/she wants to play one more game
+    // until the maximum game count is reached. This can act as a series, so
+    // that final winner is decided based on the number of matche won out of the
+    // number of  GAMES played.
     while(game->games < GAMES)
     {
         printf("Play again ? (Y/N)\n");
         if(scanf(" %c", &choice) == 1);
 
+        // if yes then start the game again with a fresh board.
         if(choice == 'y' || choice == 'Y')
         {
-            gameReset(game);
             startGame(game, player_1, player_2);
-            scoreBoard(player_1, player_2, game->games);
         }
 
         else
@@ -75,11 +78,13 @@ int main(void)
     free(player_2);
 }
 
+// assigns either 'X' or 'O' to the players.
 void assignXO(Player *player_1, Player *player_2)
 {
     char choice;
     int errorCheck;
 
+    // checks for invalid inputs.
     do
     {
         printf("Enter Player 1's name : ");
@@ -87,6 +92,7 @@ void assignXO(Player *player_1, Player *player_2)
 
     } while(errorCheck != 1);
 
+    // checks for invalid inputs.
     do
     {
         printf("Enter Player 1's name : ");
@@ -94,8 +100,9 @@ void assignXO(Player *player_1, Player *player_2)
 
     } while(errorCheck != 1);
 
-    printf("\n%s, ", player_1->name);
 
+    printf("\n%s, ", player_1->name);
+    // checks for invalid inputs.
     do
     {
         printf("X or O ?\n");
@@ -104,52 +111,67 @@ void assignXO(Player *player_1, Player *player_2)
 
     }while((choice != 'X' && choice != 'O' && choice != 'x' && choice != 'o') || errorCheck != 1);
 
-    // assigns X or O to the two players.
     player_1->choice = toupper(choice);
 
+    // assign 'X' to player_1
     if(player_1->choice == 'X')
     {
         player_1->choice = 'X';
         player_2->choice = 'O';
     }
-    
+
+    // assign 'O' to player_1
     else
     {
-        player_2->choice = 'X';
         player_1->choice = 'O';
+        player_2->choice = 'X';
     }
 
+    // first player starts the game.
     player_1->turn = true;
     player_2->turn = false;
 }
 
+// the main game loop where the moves are made and winner is decided.
 void startGame(Game *game, Player *player_1, Player *player_2)
 {
+    // resets the game board and displays it.
+    gameReset(game);
     display(game, player_1, player_2);
 
+    // continue the game till either player wins or no more moves are possible.
     while(!victoryCheck(game) && !isComplete(game))
     {
+        // player_1 makes a move if it is his/her turn.
         if(player_1->turn)
             playerMove(game, player_1);
 
+        // player_2 makes a move if it is his/her turn.
         else
             playerMove(game, player_2);
 
+        // swap the turns.
         player_1->turn = !player_1->turn;
         player_2->turn = !player_2->turn;
 
         display(game, player_1, player_2);
     }
 
+    // once the game is completed declare the winner, if any.
     declareWinner(game, player_1, player_2);
+
+    // display the scoreboard after every game.
+    scoreBoard(player_1, player_2, game->games);
 }
 
+// stored and checks the move of each player.
 void playerMove(Game *game, Player *player)
 {
     int i, j;
     int errorCheck;
     char pos;
 
+    // checks for a valid input.
     do
     {
         printf("%s's turn : ", player->name);
@@ -159,6 +181,7 @@ void playerMove(Game *game, Player *player)
 
     game->validPos[pos - '0'] = false;
 
+    // if input is valid, then store it in the board.
     for(i = 0; i < 3; i++)
         for(j = 0; j < 3; j++)
             if(game->board[i][j] == pos)
@@ -192,6 +215,7 @@ bool victoryCheck(Game *game)
     return false;
 }
 
+// checks if there are any futher possible moves.
 bool isComplete(Game *game)
 {
     int i, j;
@@ -209,11 +233,12 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
 {
     int i, j;
 
-    // checks along a row.
+    // checks for a winner along a row.
     for(i = 0, j = 0; i < 3; i++)
     {
         if(game->board[i][0] == game->board[i][1] && game->board[i][0] == game->board[i][2])
         {
+            // if winner is player_1.
             if(game->board[i][j] == player_1->choice)
             {
                 printf("%s Wins!\n", player_1->name);
@@ -221,6 +246,7 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
                 player_2->score[game->games] = LOSS;
             }
 
+            // if winner is player_2.
             else
             {
                 printf("%s Wins!\n", player_2->name);
@@ -233,11 +259,12 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
         }
     }
 
-    // checks along a coloumn.
+    // checks for a winner along a coloumn.
     for(j = 0, i = 0; j < 3; j++)
     {
         if(game->board[0][j] == game->board[1][j] && game->board[0][j] == game->board[2][j])
         {
+            // if winner is player_1.
             if(game->board[0][j] == player_1->choice)
             {
                 printf("%s Wins!\n", player_1->name);
@@ -245,6 +272,7 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
                 player_2->score[game->games] = LOSS;
             }
 
+            // if winner is player_2.
             else
             {
                 printf("%s Wins!\n", player_2->name);
@@ -260,6 +288,7 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
     // checks along right diagonal.
     if(game->board[0][0] == game->board[1][1] && game->board[0][0] == game->board[2][2])
     {
+        // if winner is player_1.
         if(game->board[0][0] == player_1->choice)
         {
             printf("%s Wins!\n", player_1->name);
@@ -267,6 +296,7 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
             player_2->score[game->games] = LOSS;
         }
 
+        // if winner is player_2.
         else
         {
             printf("%s Wins!\n", player_2->name);
@@ -297,6 +327,7 @@ void declareWinner(Game *game, Player *player_1, Player *player_2)
         return;
     }
 
+    // if there were no winners then the result was a draw.
     if(isComplete(game))
     {
         printf("Game Over !\n");
@@ -338,6 +369,7 @@ void display(Game *game, Player *player_1, Player * player_2)
     printf("%s - %c\n", player_2->name, player_2->choice);
 }
 
+// reset the valid positions and the board.
 void gameReset(Game *game)
 {
     int i, j;
@@ -356,6 +388,7 @@ void gameReset(Game *game)
     }
 }
 
+// display and calculate the score board.
 void scoreBoard(Player *player_1, Player *player_2, int games)
 {
     int i;
@@ -368,6 +401,7 @@ void scoreBoard(Player *player_1, Player *player_2, int games)
     printf("   Game (%d / %d)\n", games, GAMES);
     printf("==================\n");
 
+    // caculate the score for each player.
     for(i = 0; i < games; i++)
     {
         totalScore1 += player_1->score[i];
@@ -377,29 +411,33 @@ void scoreBoard(Player *player_1, Player *player_2, int games)
 
     printf("%s[%d]  %s[%d]\n\n", player_1->name, totalScore1, player_2->name, totalScore2);
 
+    // display the score on the screen with colour.
     for(i = 0; i < games; i++)
     {
         if(player_1->score[i] == WIN)
+        {
             printf(ANSI_COLOR_GREEN "  %2d", WIN);
-
-        else
+            printf(ANSI_COLOR_RESET "\t");
             printf(ANSI_COLOR_RED "  %2d", LOSS);
+        }
 
-        printf(ANSI_COLOR_RESET "\t");
-
-        if(player_2->score[i] == WIN)
-            printf(ANSI_COLOR_GREEN "   %2d", WIN);
-
+        else if (player_1->score[i] == LOSS)
+        {
+            printf(ANSI_COLOR_RED "  %2d", LOSS);
+            printf(ANSI_COLOR_RESET "\t");
+            printf(ANSI_COLOR_GREEN "  %2d", WIN);
+        }
 
         else
-            printf(ANSI_COLOR_RED "   %2d", LOSS);
+            printf("  %2d\t  %2d", DRAW, DRAW);
 
         printf(ANSI_COLOR_RESET "\n");
     }
 
+    //  display the series result.
     printf("==================\n");
     printf("\n");
-    if(games + 1 > GAMES)
+    if(games == GAMES)
     {
         if(totalScore1 > totalScore2)
             printf("%s WINS THE SERIES !\n", player_1->name);
